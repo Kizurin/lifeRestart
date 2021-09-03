8ffdc392931a49c6cac41c69a877bfd191ef3638
Kizurin <38313797+Kizurin@users.noreply.github.com>
2021-09-03 18:13:17 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -128,7 +128,7 @@ class App{
                 <div id="total" style="font-size:1rem; font-weight:normal;">可用属性点：0</div>
             </div>
             <ul id="propertyAllocation" class="propinitial"></ul>
-            <button id="random" class="mainbtn" style="top:auto; bottom:7rem" disabled="disabled">随机分配坏了</button>
+            <button id="random" class="mainbtn" style="top:auto; bottom:7rem">随机分配坏了</button>
             <button id="start" class="mainbtn" style="top:auto; bottom:0.1rem">开始新人生</button>
         </div>
         `);
@@ -200,7 +200,24 @@ class App{
 
         propertyPage
             .find('#random')
-            .click();
+            .click(()=>{
+                let t = this.#totalMax;
+                const arr = [10, 10, 10, 10];
+                while(t>0) {
+                    const sub = Math.round(Math.random() * (Math.min(t, 10) - 1)) + 1;
+                    while(true) {
+                        const select = Math.floor(Math.random() * 4) % 4;
+                        if(arr[select] - sub <0) continue;
+                        arr[select] -= sub;
+                        t -= sub;
+                        break;
+                    }
+                }
+                groups.CHR.set(10 - arr[0]);
+                groups.INT.set(10 - arr[1]);
+                groups.STR.set(10 - arr[2]);
+                groups.MNY.set(10 - arr[3]);
+            });
 
         propertyPage
             .find('#start')
